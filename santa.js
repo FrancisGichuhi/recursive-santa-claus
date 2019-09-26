13df1a5e90134157c4430dc61acd87d1a8b69b96
Francis Gichuhi <francisgichh@gmail.com>
2019-09-25 21:30:48 -0400

    Santa Assignment

--- santa.js
+++ santa.js
@@ -1,8 +1,8 @@
 function deliverPresents(houses) {
   if (houses.length === 0) return
 
-  first = houses[0]
-  rest = houses.slice(1, houses.lenth)
+  var first = houses[0]
+  var rest = houses.slice(1, houses.lenth)
   console.log('Delivering presents to ' + first)
   deliverPresents(rest)
 }
